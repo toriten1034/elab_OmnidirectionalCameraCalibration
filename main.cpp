0c2c9a85c477e7161ce3c9a396b0ede2c125b306
toriten1034 <umitakamru24@gmail.com>
2018-01-16 15:09:12 +0900

    camera tuner

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/stitching.hpp>
-#include "OmnirangeCamera.hpp"
-
+#include "OmnidirectionalCamera.hpp"
 
 int HorizonalCrossCount = 9;
 int VerticalCrossCount = 6;
@@ -32,16 +31,16 @@ void ImgDiv(cv::Mat src, cv::Mat right, cv::Mat left){
   }
 }
 
-void ImgJoin(cv::Mat right, cv::Mat left, cv::Mat src){
+void ImgJoin(cv::Mat right, cv::Mat left, cv::Mat src, int hdiff){
   int width = src.cols/2;
   int height = src.rows;
-  for(int i = 0; i < height; i++){
-    cv::Vec3b *p_src   = src.ptr<cv::Vec3b>(i);
-    cv::Vec3b *p_right = right.ptr<cv::Vec3b>(i);
-    cv::Vec3b *p_left  = left.ptr<cv::Vec3b>(i);
-    for(int j = 0; j < width; j++){
-      p_src[j] = p_left[j];
-      p_src[j + width] = p_right[j] ;
+  for(int y = 0; y < height - hdiff; y++){
+    cv::Vec3b *p_src   = src.ptr<cv::Vec3b>(y);
+    cv::Vec3b *p_right = right.ptr<cv::Vec3b>(y+((hdiff < 25)? hdiff : 0 ));
+    cv::Vec3b *p_left  = left.ptr<cv::Vec3b>(y+((hdiff > 25)? hdiff : 0));
+    for(int x = 0; x < width; x++){
+      p_src[x] = p_left[x];
+      p_src[x + width] = p_right[x];
     }
   }
 }
@@ -168,7 +167,7 @@ void xMap3(cv::Rect src,cv::Mat dst){
       double r_angle = asin(r/(height/2));
       //correct by focal length
       //double f = (height/height/2)/sin(r_angle/2)/2;
-      double fr = tan(r_angle/2);
+      double fr = sin(r_angle);
 
       double rate = fr/(r/(height/2));
       
@@ -209,7 +208,7 @@ void yMap3(cv::Rect src,cv::Mat dst){
       double r_angle = asin(r/(height/2));
       //correct by focal length
       // double f = (height/height/2)/sin(r_angle)/2;
-      double fr = tan(r_angle/2);
+      double fr = sin(r_angle);
 
       double rate = fr/(r/(height/2));
       
@@ -227,30 +226,6 @@ void yMap3(cv::Rect src,cv::Mat dst){
 }
 
 
-
-
-void omniremap(cv::Mat src, cv::Mat dst, cv::Mat xmap, cv::Mat ymap){
-  int src_width = src.cols;
-  int dst_width = dst.cols;
-  if(xmap.cols != dst.cols || xmap.rows != dst.rows || ymap.cols != dst.cols || ymap.rows != dst.rows){
-    std::cout << "error" << std::endl;
-  }
-  
-  int height = xmap.rows;
-  
-  cv::Vec3b *p_src = src.ptr<cv::Vec3b>(0);      
-  
-  for(int y = 0 ; y < height; y++){
-    cv::Vec3b *p_dst = dst.ptr<cv::Vec3b>(y);
-    unsigned short *p_xmap  = xmap.ptr<unsigned short>(y);
-    unsigned short *p_ymap  = ymap.ptr<unsigned short>(y);
-    for(int x = 0 ; x < dst_width; x++){
-      int index = p_ymap[x] * src_width + p_xmap[x];
-      p_dst[x] = p_src[index];
-    }
-  }
-}
-
 int main(int argc , char* argv[]){
   if(argc != 2){
     std::cout << "too few argments" << std::endl;
@@ -258,7 +233,8 @@ int main(int argc , char* argv[]){
   }
 
   int cam_id = std::atoi(argv[1]); 
-  int clip = 0; 
+  int clip = 0;
+  int hdiff = 0;
   std::cout << "s to capture" << std::endl;
   std::cout << "ESC s to Exit Program" << std::endl;
 
@@ -270,64 +246,68 @@ int main(int argc , char* argv[]){
   cv::Mat src;
   insta360 >> src;
   cv::Rect roi = cv::Rect(clip,clip,src.cols/2 - (2*clip) ,src.rows - (2*clip) );
-  int change = 1;
 
+  const std::string WinName = "Joind";
+  int key = 0;
+
+  int clip_t = 0;;
+  int hdiff_t = 0;
   while(1){
-
-    int key = cv::waitKey(1);
     if(key == 49){
       if(0 < clip ){
 	clip--;
-	std::cout << "clip range is " << clip <<std:: endl;
-	roi = cv::Rect(clip,clip,src.cols/2 - (2*clip) ,src.rows - (2*clip) );
-	change = 1;
       }  
     }else if (key == 50){
       if(0 < 1000 ){
 	clip++;
-	std::cout << "clip range is " << clip <<std:: endl;
-	roi = cv::Rect(clip,clip,src.cols/2 - (2*clip) ,src.rows - (2*clip) );
-	change = 1;
-      }
+   }
     }else if(key == 27){
       break;
     }
-    
+    std::cout << "clip range is " << clip <<std:: endl;
+    roi = cv::Rect(clip,clip,src.cols/2 - (2*clip) ,src.rows - (2*clip) );
+   
     cv::Mat y_mat(roi.width, roi.height,CV_16UC1);
     cv::Mat x_mat(roi.width, roi.height,CV_16UC1);
-    
-    xMap3(roi,x_mat); 
-    yMap3(roi,y_mat); 
-    
-    if(change == 1){
-      std::cout << "change" << std::endl;
+    OmnidirectionalCamera::OmnirangeCameraRemapperGen(roi, x_map, y_map , OmnidirectionalCamera::ORTHOGRAPHIC);
       xMap3(roi,x_mat); 
-      yMap3(roi,y_mat); 
-      change = 0;      
+    yMap3(roi,y_mat); 
+
+    while(1){  
+            
+      //fetch image
+      insta360 >> src;
+      cv::Mat right(src.cols/2, src.rows,right.type());
+      cv::Mat left(src.cols/2, src.rows,right.type()); 
+      ImgDiv(src,right,left);
+      cv::Mat cliped_r(roi.width, roi.height,src.type());
+      cv::Mat cliped_l(roi.width, roi.height,src.type());
+      
+      ImgClip(right,cliped_r,roi);
+      ImgClip(left,cliped_l,roi);
+      cv::Mat result_right(x_mat.rows, x_mat.cols, src.type());
+      cv::Mat result_left(x_mat.rows, x_mat.cols, src.type());
+      
+      // cv::imshow("debug2",cliped_r);
+      
+      OmnidirectionalCamera::OmnidirectionalImageRemap(cliped_r, result_right,x_mat,y_mat);
+      OmnidirectionalCamera::OmnidirectionalImageRemap(cliped_l, result_left,x_mat,y_mat);
+      
+      cv::Mat Join( result_right.rows -hdiff,result_right.cols*2,result_right.type());
+      ImgJoin(result_right,result_left,Join ,hdiff);
+      cv::imshow(WinName,Join);
+      cv::createTrackbar("Clip",WinName,&clip,50);
+      cv::createTrackbar("Height diff",WinName,&hdiff,50);
+      key = cv::waitKey(1);
+      
+      // if input any key 
+      if(key != 255 || clip_t != clip){
+	clip_t = clip;
+	hdiff_t = hdiff;
+	break;
+      }
     }
-    
-    //fetch image
-    insta360 >> src;
-    cv::Mat right(src.cols/2, src.rows,right.type());
-    cv::Mat left(src.cols/2, src.rows,right.type()); 
-    ImgDiv(src,right,left);
-    cv::Mat cliped_r(roi.width, roi.height,src.type());
-    cv::Mat cliped_l(roi.width, roi.height,src.type());
-    
-    ImgClip(right,cliped_r,roi);
-    ImgClip(left,cliped_l,roi);
-    
-    cv::Mat result_right(x_mat.rows, x_mat.cols, src.type());
-    cv::Mat result_left(x_mat.rows, x_mat.cols, src.type());
-       
-    cv::imshow("debug2",cliped_r);
-    
-    omniremap(cliped_r, result_right,x_mat,y_mat);
-    omniremap(cliped_l, result_left,x_mat,y_mat);
-    
-    cv::Mat Join(result_right.rows ,result_right.cols*2, result_right.type() );
-    ImgJoin(result_right,result_left,Join);
-    cv::imshow("result_r",Join);
-    
+   
   }
+
 }
